287943704daaa98707bc7acd719d734101cf4dbd
teacher-min <minkt78@gmail.com>
2024-09-29 14:07:20 +0900

    second commit

---
+++ src/main/java/ac/kr/koreatech/demo/DemoController.java
@@ -0,0 +1,15 @@
+package ac.kr.koreatech.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class DemoController {
+
+  @GetMapping("/")
+  public String index() {
+      return "index.html";
+  }
+
+}
